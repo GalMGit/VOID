3ae1097d3d0ae19280e3a4051a78cd8c540ad027
GalMGit <loguhiy.f@gmail.com>
2026-08-19 19:43:07 +0400

    test endpoint

--- src/backend/VOID.API/Endpoints/Tests/Test.cs
+++ src/backend/VOID.API/Endpoints/Tests/Test.cs
@@ -2,14 +2,10 @@ using VOID.API.EndpointsConfig;
 
 namespace VOID.API.Endpoints.Tests;
 
-public sealed class Test : IEndpoint
+public class Test : IEndpoint
 {
     public void MapEndpoint(IEndpointRouteBuilder app)
     {
-        app.MapGet("test", () => Results.Ok(new
-            {
-                Success = "endpoint has been tested."
-            }))
-            .AllowAnonymous();
+        app.MapGet("test", () => "123").AllowAnonymous();
     }
 }
